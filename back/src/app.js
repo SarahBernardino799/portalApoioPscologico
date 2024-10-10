5d4cd9dbcd0a03e9ef1431d142cb975cf43eef37
sarahbernardino799 <sarahberna799@gmail.com>
2024-10-10 16:32:53 -0300

    inicio

--- back/src/app.js
+++ back/src/app.js
@@ -14,5 +14,9 @@ const cadastroRotas = require("./rotas/cadastroRotas.js");
 app.use("/sistema/adm",admRotas);
 app.use("/sistema/cadastro",cadastroRotas);
 
+const swaggerUi = require("swagger-ui-express")
+const swaggerFile = require("../swagger/swagger_output.json")
+app.use("/minha-documentacao", swaggerUi.serve, swaggerUi.setup(swaggerFile))
+
 
 module.exports = app;
